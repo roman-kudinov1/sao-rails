ae7d84999b20ef8929c52fb3e3ced2cb679c2c9a
Daniel Costa <costa@vixinet.ch>
2017-11-06 18:09:31 +0200

    init

--- db/migrate/20171106144855_add_default_value_to_status.rb
+++ db/migrate/20171106144855_add_default_value_to_status.rb
@@ -1,4 +1,4 @@
-class AddDefaultValueToProspect < ActiveRecord::Migration[5.1]
+class AddDefaultValueToStatus < ActiveRecord::Migration[5.1]
 	def up
 		change_column :prospects, :status, :integer, default: 0
 	end
