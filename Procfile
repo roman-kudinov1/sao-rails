9717c9ddcafe59cac0334ec59b8616207f20bb30
Daniel Costa <costa@vixinet.ch>
2017-11-08 17:04:23 +0200

    changes

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
-worker: bundle exec sidekiq -q default -c 3
+worker: bundle exec sidekiq -q default 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 	  post '/actions/get-sa' => 'prospects#harvest_sa'
 	  post '/actions/get-sarl' => 'prospects#harvest_sarl'
 	end
+	
+	require 'sidekiq/web'
+	mount Sidekiq::Web => '/sidekiq'
 
   mount ForestLiana::Engine => '/forest'
 
